509e957620d54d36d9a07b2dca846bcf31e383d0
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2025-08-21 19:58:30 +0530

    bug fixes

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,13 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv";
-import { app } from "./app.js";
-dotenv.config({
-  path: "./.env",
-});
+import http from "http";
+import app from "./app.js";
+import initSocket from "./socket/index.js";
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT, "0.0.0.0", () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-  });
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+initSocket(server);
+
+server.listen(port, (req, res) => {
+  console.log("server is listening at port no :", port);
+});
